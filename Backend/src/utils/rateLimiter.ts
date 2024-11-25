efd249dcfa995fe2237a748639b7add21ed70e4f
afsal <madathingalafsal@gmail.com>
2024-11-25 17:32:45 +0530

    rate limit

--- Backend/src/utils/rateLimiter.ts
+++ Backend/src/utils/rateLimiter.ts
@@ -3,13 +3,15 @@ import { Request, Response } from 'express';
 
 // Create a rate limiter instance
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
-  max: 10, 
-  message: 'What you want from my application submit on afsalmadathingal.online i will give you that :(',
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 5 requests per windowMs
   handler: (req: Request, res: Response) => {
-    res.status(429).json({ message: 'You are being rate-limited. Try again later.' });
+    res.status(429).json({
+      message: `Oh, trying to grab some files, are we? Go ahead, keep flooding the app—I'm sure that'll work out perfectly for you!`,
+    });
   },
 });
+
 
 // Export the limiter for use in other parts of your app
 export default limiter;
