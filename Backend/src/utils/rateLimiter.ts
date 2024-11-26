139dc8d11981320d1bce574775160df6ace7f874
afsal <madathingalafsal@gmail.com>
2024-11-26 21:22:40 +0530

    rate limit reset

--- Backend/src/utils/rateLimiter.ts
+++ Backend/src/utils/rateLimiter.ts
@@ -4,11 +4,11 @@ import { Request, Response } from 'express';
 // Create a rate limiter instance
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 5 requests per windowMs
+  max: 500, // Limit each IP to 5 requests per windowMs
   handler: (req: Request, res: Response) => {
     console.log(`Rate limit exceeded: ${req.method} ${req.url}`)
     res.status(429).json({
-      message: `this is a boot camp project ur on the wrong place`,
+      message: `You are Blocked`,
     });
   },
 });
